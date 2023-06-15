70c45b3abaccf0d8df7a4fd10639f8db02074078
Lihuel <lihuelsalvo@gmail.com>
2023-06-14 23:11:25 -0300

    login armado - falta permisos

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -6,7 +7,13 @@ urlpatterns = [
    
     path("",views.index, name ="index"),
     path("laliga",views.laliga, name ="laliga"),
-    path("login",views.login, name ="login"),
+    path("login",views.login_request, name ="login"),
+    path("registro",views.registro, name ="registro"),
+    path("logout", LogoutView.as_view(template_name="home/logout.html"), name="logout"),
+    path("nosotros",views.nosotros, name ="nosotros"),
+   
+    
+
     # path("noticias",views.noticias, name ="noticias"),
 ]
 
--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from datetime import datetime
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from . import forms
+
 
 def index(request):
     return render(request, "home/index.html")
@@ -9,8 +13,35 @@ def laliga(request):
     Fecha = now.strftime("%d de  %B del %Y ")
     return render(request, "home/laliga.html", {"Fecha": Fecha})
 
-def login(request):
-    return render(request, "home/login.html")
+
+def nosotros(request):
+    return render(request, "home/nosotros.html")
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = forms.CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None:
+                login(request, user)
+                return render(request, "home/index.html", {"mensaje": f"Usted a sido logeado como {usuario}"})
+    else:
+        form = AuthenticationForm()
+    return render(request, "home/login.html", {"form": form})
+
+def registro(request):
+    if request.method == "POST":
+        form = forms.CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            return render(request, "home/index.html", {"Usuario": username})
+    else:
+        form = forms.CustomUserCreationForm()
+    return render(request, "home/registro.html", {"form": form})
 
 # def noticias(request):
 #     return render(request, "noticias/index.html")
